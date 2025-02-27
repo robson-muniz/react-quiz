c0615f9bc80bb2fd3a7f81a24e6a706397b4f50d
robsonmuniz <robson_muniz@yahoo.com>
2025-02-27 19:13:19 +0000

    Loading question from a Fake API and use UseReducer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
-import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Main from "./Main";
+import {useEffect, useReducer} from "react";
+
+const initialState = {
+  questions: [],
+  //'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      }
+
+      case 'dataFailed':
+        return {
+          ...state,
+          status: "failed",
+        }
+      default:
+        throw new Error('action unknown');
+  }
+
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions") // Ensure your backend is running
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => dispatch({type: "dataReceived", payload: data}))
+      .catch((error) => dispatch({type: 'dataFailed'}));
+  }, []); // Runs only once when the component mounts
+
   return (
-    <div>
-      <DateCounter />
+    <div className="App">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
