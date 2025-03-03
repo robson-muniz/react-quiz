7c181145572b5c41f207f47d5e98114684005ca0
robsonmuniz <robson_muniz@yahoo.com>
2025-03-03 10:25:04 +0000

    008 Starting a New Quiz

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {useEffect, useReducer} from "react";
 import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
+import Question from "./Question";
 
 const initialState = {
   questions: [],
@@ -25,6 +26,13 @@ function reducer(state, action) {
           ...state,
           status: "failed",
         }
+
+    case 'start':
+      return {
+        ...state,
+        status: "active",
+      }
+
       default:
         throw new Error('action unknown');
   }
@@ -54,7 +62,8 @@ function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} dispatch={dispatch} />}
+        {status === "active" && <Question />}
       </Main>
     </div>
   );
---
+++ src/Question.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Question(props) {
+  return (
+    <div>
+      Question
+    </div>
+  );
+}
+
+export default Question;
